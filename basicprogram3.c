d5ce86869752c849d2447a8960a038e8cee95fa7
5zulahamed <112261054+5zulahamed@users.noreply.github.com>
2022-09-07 20:35:47 +0530

    Add files via upload

---
+++ basicprogram3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int first;
+    float second;
+    printf("Please enter a int and float..:");
+    scanf("%d%f",&first,&second);
+    printf("The first value is..:%d and second value is..:%f",first,second);
+}
