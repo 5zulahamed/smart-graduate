727147602a10dd8fddc5f67864309f2ac6e2ae47
5zulahamed <112261054+5zulahamed@users.noreply.github.com>
2022-09-26 18:16:11 +0530

    Add files via upload

---
+++ CastingExercise.java
@@ -0,0 +1,16 @@
+package smartgraduates;
+
+public class CastingExercise {
+
+	public static void main(String[] args) {
+		byte b=10;
+		
+		int i=b;
+		 byte c=(byte)i;
+		 
+		 byte d=10;
+		 byte e= 20;
+		 
+	}
+	
+}
